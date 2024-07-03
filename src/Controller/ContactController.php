fa3845a30e067eaefade5957785d7c9dc413ad5f
liliannav777 <162148949+liliannav777@users.noreply.github.com>
2024-07-03 14:21:43 +0200

    Création de la page de contact et du form et de la page de services

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'contact')]
+    public function contact(Request $request): Response
+    {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->addFlash('success', 'Votre message a été envoyé avec succès.');
+
+            return $this->redirectToRoute('contact');
+        }
+
+        return $this->render('contact/contact.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
