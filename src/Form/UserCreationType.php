7d9271b0279063c5e616d1376e7d1b288f17c9cf
liliannav777 <162148949+liliannav777@users.noreply.github.com>
2024-07-16 23:32:28 +0200

    Deploiement Heroku

--- src/Form/UserCreationType.php
+++ src/Form/UserCreationType.php
@@ -4,16 +4,14 @@ namespace App\Form;
 
 use App\Entity\Role;
 use App\Entity\Utilisateur;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\OptionsResolver\Options;
 
-class UserCreationFormType extends AbstractType
+class UserCreationType extends AbstractType
 {
     private $entityManager;
 
@@ -25,20 +23,17 @@ class UserCreationFormType extends AbstractType
         $choices = [];
         foreach ($roles as $role) {
             if ($role->getLabel() !== 'ROLE_ADMIN') {
-                $choices[$role->getLabel()] = $role;
+                $choices[$role->getLabel()] = $role->getLabel();
             }
         }
 
         $builder
             ->add('username', null, ['label' => 'Nom d\'utilisateur'])
             ->add('nom', null, ['label' => 'Nom'])
-            ->add('prenom', null, ['label' => 'Prenom'])
+            ->add('prenom', null, ['label' => 'Prénom'])
             ->add('password', PasswordType::class, ['label' => 'Mot de Passe'])
             ->add('role', ChoiceType::class, [
                 'choices' => $choices,
-                'choice_label' => function ($choice) {
-                    return $choice instanceof Role ? $choice->getLabel() : '';
-                },
                 'label' => 'Rôle',
                 'placeholder' => 'Choisir un rôle',
             ]);
