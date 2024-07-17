04c376ba8fa77398eadcfaa984b41065ddaffbaa
liliannav777 <162148949+liliannav777@users.noreply.github.com>
2024-07-17 14:09:54 +0200

    Test de connexion à MongoDB Atlas

--- src/Service/MongoDBService.php
+++ src/Service/MongoDBService.php
@@ -2,16 +2,34 @@
 namespace App\Service;
 
 use MongoDB\Client;
+use Exception;
 
 class MongoDBService
 {
     private $client;
     private $database;
 
-    public function __construct(string $mongodbUrl, string $mongodbDatabase)
+    public function __construct()
     {
-        $this->client = new Client($mongodbUrl);
-        $this->database = $this->client->selectDatabase($mongodbDatabase);
+        $mongodbUri = $_ENV['MONGODB_URI'] ?? null; // Récupère l'URI depuis l'environnement
+        $mongodbDatabase = $_ENV['MONGODB_DATABASE'] ?? null; // Récupère le nom de la base de données depuis l'environnement
+
+        if (!$mongodbUri) {
+            throw new Exception('L\'URI MongoDB n\'est pas défini dans les variables d\'environnement.');
+        }
+
+        if (!$mongodbDatabase) {
+            throw new Exception('Le nom de la base de données MongoDB n\'est pas défini dans les variables d\'environnement.');
+        }
+
+        try {
+            $this->client = new Client($mongodbUri);
+            $this->database = $this->client->selectDatabase($mongodbDatabase);
+            $this->client->listDatabases(); // Vérifie la connexion
+        } catch (\Exception $e) {
+            error_log("Erreur lors de la connexion à MongoDB Atlas : " . $e->getMessage());
+            throw new Exception("Erreur lors de la connexion à MongoDB Atlas : " . $e->getMessage());
+        }
     }
 
     public function getCollection(string $collectionName)
